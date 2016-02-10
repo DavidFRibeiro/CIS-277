65e0c70edba6ee2c50fa8812f3299ea5dbcb2430
DavidFRibeiro <David@Davids-MBP-2.home>
2016-02-09 23:59:26 -0500

    Adjusted Wording.
    Changed the menu and case comment text to reflect  the new project.

--- Stacks-Program/main.cpp
+++ Stacks-Program/main.cpp
@@ -11,49 +11,49 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     int x;
     
-    cout << "Please enter the number of the operation you would like to perform" << endl << endl;
-    cout << "1. Enter new vector values" << endl;
-    cout << "2. Add" << endl;
-    cout << "3. Subtract" << endl;
-    cout << "4. Scalar Multiplication" << endl;
-    cout << "5. Scalar Product" << endl;
-    cout << "6. Magnitude of Vector 1" << endl;
-    cout << "0. Exit program" << endl;
+    cout << "YOU MUST CHOOSE!" << endl;
+    cout << "1.) Create a stack." << endl;
+    cout << "2.) Is it empty?" << endl;
+    cout << "3.) Push something on the stack." << endl;
+    cout << "4.) Pop something off the stack." << endl;
+    cout << "5.) The stack top." << endl;
+    cout << "6.) Kill the stack." << endl;
+    cout << "0.) Leave." << endl;
     
     
-    //Switch statement based on the menu options
+    //Switch statement based on the menu options.
     switch (x)
     {
-            //Get vector values from user
+            // Create a stack.
         case 1:
         //
             break;
             
-            //Calculate sum of vectors
+            //Check whether the stack is empty.
         case 2:
          //
             break;
             
-            //Calculate difference of vectors
+            //Push a record onto the stack.
         case 3:
      //
             break;
             
-            //Calculate scalar multiplication of vector 1
+            //Pop a record off of the stack.
         case 4:
         //
             break;
             
-            //Calculate scalar product
+            //Get the current top of the stack.
         case 5:
           //
             break;
             
-            //Calculate magnitude of vector 1
+            //Purge the stack contents
         case 6:
            //
             break;
-            //Stops the loop and exits the program
+            //Quit the program.
         case 0:
             
             break;
